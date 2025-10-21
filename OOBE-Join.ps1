d908e79be481a0e2d5da300ae2f19efeaf440ff4
Jegwebb <137265604+Jegwebb@users.noreply.github.com>
2025-10-21 21:21:01 +0100

    Update OOBE-Join.ps1

--- OOBE-Join.ps1
+++ OOBE-Join.ps1
@@ -1,5 +1,8 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 Install-PackageProvider -Name NuGet -Force
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 Install-Script -Name Get-WindowsAutopilotInfo -Force
 Get-WindowsAutopilotInfo -Online
+
